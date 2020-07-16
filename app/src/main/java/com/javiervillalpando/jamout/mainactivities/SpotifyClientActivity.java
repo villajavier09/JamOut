b87621f29e33f859dade776613123aa53b2549b7
Javier Villalpando <villajavier@fb.com>
2020-07-16 17:13:26 -0400

    Added spotify authentication

---
+++ app/src/main/java/com/javiervillalpando/jamout/mainactivities/SpotifyClientActivity.java
@@ -0,0 +1,73 @@
+package com.javiervillalpando.jamout.mainactivities;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.javiervillalpando.jamout.R;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
+public class SpotifyClientActivity extends AppCompatActivity {
+
+    private SharedPreferences.Editor editor;
+    private SharedPreferences msharedPreferences;
+    private RequestQueue queue;
+
+    private static final String CLIENT_ID = "47031eb52a5d4042816c26f45cfb1950";
+    private static final String REDIRECT_URI = "com.javiervillalpando.jamout://callback";
+    private static final int REQUEST_CODE = 1337;
+    private static final String SCOPES = "user-read-recently-played,user-library-modify,user-read-email,user-read-private";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_spotify_client);
+
+        authenticateSpotify();
+
+        msharedPreferences = this.getSharedPreferences("SPOTIFY",0);
+        queue = Volley.newRequestQueue(this);
+    }
+
+    private void authenticateSpotify() {
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+        builder.setScopes(new String[]{SCOPES});
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if(requestCode == REQUEST_CODE){
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode,intent);
+
+            switch (response.getType()){
+                case TOKEN:
+                    editor = getSharedPreferences("SPOTIFY",0).edit();
+                    editor.putString("token", response.getAccessToken());
+                    Log.d("STARTING", "GOT AUTH TOKEN");
+                    editor.apply();
+                    ///waitForUserInfo();
+                    break;
+                case ERROR:
+                    break;
+                default:
+
+            }
+
+        }
+    }
+}
--- app/src/main/java/com/javiervillalpando/jamout/mainactivities/profile/ProfileFragment.java
+++ app/src/main/java/com/javiervillalpando/jamout/mainactivities/profile/ProfileFragment.java
@@ -14,6 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import com.google.android.material.button.MaterialButton;
 import com.javiervillalpando.jamout.mainactivities.LoginActivity;
 import com.javiervillalpando.jamout.R;
 import com.parse.ParseUser;
